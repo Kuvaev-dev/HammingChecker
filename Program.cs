a26ba53e16c2c42d0f7ed5970d77cfe74d2603b9
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2023-11-09 23:03:57 +0200

    Design Elements Added

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         // Встановлюємо кодування консолі на Unicode та змінюємо колір тексту на зелений.
         Console.OutputEncoding = Encoding.Unicode;
@@ -14,102 +14,106 @@ class Program
         while (true)
         {
             // Виводимо меню опцій.
-            Console.WriteLine("1. Кодувати");
-            Console.WriteLine("2. Декодувати");
-            Console.WriteLine("3. Внести помилку");
-            Console.WriteLine("4. Вийти");
-            Console.Write("Введіть номер опції: ");
+            Console.WriteLine("╭───────────────────────────────╮");
+            Console.WriteLine("│              Меню             │");
+            Console.WriteLine("├───────────────────────────────┤");
+            Console.WriteLine("│  1. Кодувати                  │");
+            Console.WriteLine("│  2. Декодувати                │");
+            Console.WriteLine("│  3. Внести помилку            │");
+            Console.WriteLine("│  4. Вийти                     │");
+            Console.WriteLine("╰───────────────────────────────╯");
+            Console.Write("-> Введіть номер опції: ");
             string option = Console.ReadLine();
 
             switch (option)
             {
                 case "1":
                     // Опція кодування.
-                    Console.Write("Введіть шлях до вихідного файлу: ");
+                    Console.Write("-> Введіть шлях до вихідного файлу: ");
                     string sourcePath = Console.ReadLine();
                     if (!File.Exists(sourcePath))
                     {
-                        Console.WriteLine("Файл не існує. Спробуйте ще раз.");
+                        Console.WriteLine("-> ПОМИЛКА: Файл не існує. Спробуйте ще раз.");
                         break;
                     }
 
                     // Зчитуємо байти з вихідного файлу.
                     byte[] sourceBytes = File.ReadAllBytes(sourcePath);
-                    Console.WriteLine("Початкові байти:");
+                    Console.WriteLine("-> Початкові байти:");
                     PrintBytes(sourceBytes);
 
                     // Виконуємо кодування і зберігаємо закодовані байти в інший файл.
                     byte[] encodedBytes = hammingEncoder.Encode(sourceBytes);
-                    Console.Write("Введіть шлях для збереження закодованого файлу: ");
+                    Console.Write("-> Введіть шлях для збереження закодованого файлу: ");
                     string encodedPath = Console.ReadLine();
                     File.WriteAllBytes(encodedPath, encodedBytes);
-                    Console.WriteLine("Файл успішно закодовано.");
-                    Console.WriteLine("Закодовані байти:");
+                    Console.WriteLine("-> Файл успішно закодовано.");
+                    Console.WriteLine("-> Закодовані байти:");
                     PrintBytes(encodedBytes);
 
                     break;
                 case "2":
                     // Опція декодування.
-                    Console.Write("Введіть шлях до закодованого файлу: ");
+                    Console.Write("-> Введіть шлях до закодованого файлу: ");
                     string toDecodePath = Console.ReadLine();
                     if (!File.Exists(toDecodePath))
                     {
-                        Console.WriteLine("Файл не існує. Спробуйте ще раз.");
+                        Console.WriteLine("-> ПОМИЛКА: Файл не існує. Спробуйте ще раз.");
                         break;
                     }
 
                     // Зчитуємо закодовані байти з файлу.
                     byte[] toDecodeBytes = File.ReadAllBytes(toDecodePath);
-                    Console.WriteLine("Закодовані байти:");
+                    Console.WriteLine("-> Закодовані байти:");
                     PrintBytes(toDecodeBytes);
 
                     // Виконуємо декодування і зберігаємо декодовані байти в інший файл.
                     byte[] decodedBytes = hammingEncoder.Decode(toDecodeBytes);
-                    Console.Write("Введіть шлях для збереження декодованого файлу: ");
+                    Console.Write("-> Введіть шлях для збереження декодованого файлу: ");
                     string decodedPath = Console.ReadLine();
                     File.WriteAllBytes(decodedPath, decodedBytes);
-                    Console.WriteLine("Файл успішно декодовано.");
-                    Console.WriteLine("Декодовані байти:");
+                    Console.WriteLine("-> Файл успішно декодовано.");
+                    Console.WriteLine("-> Декодовані байти:");
                     PrintBytes(decodedBytes);
 
                     break;
                 case "3":
                     // Опція введення помилки.
-                    Console.Write("Введіть шлях до закодованого файлу: ");
+                    Console.Write("-> Введіть шлях до закодованого файлу: ");
                     string errorPath = Console.ReadLine();
                     if (!File.Exists(errorPath))
                     {
-                        Console.WriteLine("Файл не існує. Спробуйте ще раз.");
+                        Console.WriteLine("-> ПОМИЛКА: Файл не існує. Спробуйте ще раз.");
                         break;
                     }
 
                     // Зчитуємо закодовані байти з файлу.
                     byte[] errorBytes = File.ReadAllBytes(errorPath);
-                    Console.Write("Введіть позицію помилки: ");
+                    Console.Write("-> Введіть позицію помилки: ");
                     int errorPosition;
                     if (!int.TryParse(Console.ReadLine(), out errorPosition) || errorPosition < 0 || errorPosition >= errorBytes.Length * 8)
                     {
-                        Console.WriteLine("Невірна позиція помилки. Спробуйте ще раз.");
+                        Console.WriteLine("-> Невірна позиція помилки. Спробуйте ще раз.");
                         break;
                     }
 
                     // Вводимо помилку в байти та зберігаємо в інший файл.
                     byte[] errorIntroducedBytes = hammingEncoder.IntroduceError(errorBytes, errorPosition);
-                    Console.Write("Введіть шлях для збереження файлу з помилкою: ");
+                    Console.Write("-> Введіть шлях для збереження файлу з помилкою: ");
                     string errorIntroducedPath = Console.ReadLine();
                     File.WriteAllBytes(errorIntroducedPath, errorIntroducedBytes);
-                    Console.WriteLine("Помилка введена.");
-                    Console.WriteLine("Байти з помилкою:");
+                    Console.WriteLine("-> Помилка введена.");
+                    Console.WriteLine("-> Байти з помилкою:");
                     PrintBytes(errorIntroducedBytes);
-                    Console.WriteLine("Позиція помилки: " + errorPosition);
+                    Console.WriteLine("-> Позиція помилки: " + errorPosition);
 
                     // Визначаємо та виправляємо помилку у байтах.
                     byte[] correctedBytes = hammingEncoder.DetectAndCorrectError(errorIntroducedBytes);
-                    Console.Write("Введіть шлях для збереження виправленого файлу: ");
+                    Console.Write("-> Введіть шлях для збереження виправленого файлу: ");
                     string correctedPath = Console.ReadLine();
                     File.WriteAllBytes(correctedPath, correctedBytes);
-                    Console.WriteLine("Помилка виправлена.");
-                    Console.WriteLine("Виправлені байти:");
+                    Console.WriteLine("-> Помилка виправлена.");
+                    Console.WriteLine("-> Виправлені байти:");
                     PrintBytes(correctedBytes);
 
                     break;
@@ -117,7 +121,7 @@ class Program
                     // Вихід із програми.
                     return;
                 default:
-                    Console.WriteLine("Невірний вибір. Спробуйте ще раз.");
+                    Console.WriteLine("-> ПОМИЛКА: Невірний вибір. Спробуйте ще раз.");
                     break;
             }
         }
@@ -125,13 +129,13 @@ class Program
 
     static void PrintBytes(byte[] bytes)
     {
-        Console.WriteLine("---------------------------------");
-        Console.WriteLine("| Index       | Byte Data       |");
-        Console.WriteLine("---------------------------------");
+        Console.WriteLine("╭-─────────────────────────────-╮");
+        Console.WriteLine("│ Index       │ Byte Data       │");
+        Console.WriteLine("├───────────────────────────────┤");
         for (int i = 0; i < bytes.Length; i++)
         {
-            Console.WriteLine("| " + i.ToString().PadRight(12) + "| " + Convert.ToString(bytes[i], 2).PadLeft(8, '0') + "        |");
+            Console.WriteLine("│ " + i.ToString().PadRight(12) + "│ " + Convert.ToString(bytes[i], 2).PadLeft(8, '0') + "        │");
         }
-        Console.WriteLine("---------------------------------");
+        Console.WriteLine("╰-─────────────────────────────-╯");
     }
 }
